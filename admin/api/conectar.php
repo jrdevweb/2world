b647051109fc3105e2eb2db81addaece113aa97a
Junior Ribeiro <jr.ribeiro.atm@gmail.com>
2019-11-13 09:19:10 -0300

    Update conectar.php

--- admin/api/conectar.php
+++ admin/api/conectar.php
@@ -1,5 +1,5 @@
 <?php
- $connect = mysqli_connect("localhost", "u339117845_novo", "Jr10041992Jr", "u339117845_novo");
+ $connect = mysqli_connect("localhost", "u339117845_2world", "Jr10041992Jr", "u339117845_2world");
    // $connect = mysqli_connect("localhost", "root", "", "2world");
 mysqli_set_charset($connect,"utf8");
 
